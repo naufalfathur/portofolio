6a1b3b7a957cd2fdee3d9ea72d6c9661b9bdb21f
naufalfathur <60430072+naufalfathur@users.noreply.github.com>
2024-03-19 02:42:51 +0000

    v2 initialization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import { Poppins } from 'next/font/google'
+import { Inter } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
-import { ThemeProvider } from "@/components/theme-provider";
-import Navbar from "@/components/navbar";
-import FlareCursor from "@/components/custom-cursor";
 
-const poppins = Poppins({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-poppins',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,23 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      
-      <body
-        className={cn(
-          `min-h-screen bg-background font-sans antialiased`,
-          poppins.className
-        )}
-      ><FlareCursor/><ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        enableSystem
-        disableTransitionOnChange
-      >
-          <>
-            <Navbar />
-            {children}
-          </>
-        </ThemeProvider></body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
