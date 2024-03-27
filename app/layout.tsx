6bef649078671bbbb62decb0ecc2d1ea286bd26e
naufalfathur <60430072+naufalfathur@users.noreply.github.com>
2024-03-27 07:58:28 +0000

    test icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const figtree = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Naufal Fathur",
   description: "Software Developer with Interactive Design Background",
+  icons: {
+    icon: "/img/favicon.png",
+  },
 };
 
 export default function RootLayout({
@@ -16,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" >
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <script src='script.js' defer></script>
       <body className={figtree.className}>
         {children}
