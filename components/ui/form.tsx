6c9bf28c303c36ae1b1120ff8585f2ff215c1cce
naufalfathur <60430072+naufalfathur@users.noreply.github.com>
2024-05-11 08:49:31 +0000

    simple send email form

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { InputWithLabel } from './input'
 import { SendHorizontal } from 'lucide-react'
 
@@ -10,15 +10,22 @@ interface FormProps {
 
 
 function Form({ title }: FormProps) {
+    const [subject, setSubject] = useState("")
+    const [message, setMessage] = useState("")
+
+    function sendEmail(){
+        window.open(`mailto:fathur@generatif.co?subject=${subject}&body=${message}`, '_blank', 'noopener,noreferrer');
+      }
+
+
     return (
         <div className='flex flex-col space-y-2 md:w-full'>
             <div className="chat chat-start">
                 <div className=" text-3xl font-extrabold  chat-bubble">{title}</div>
             </div>
-            <InputWithLabel title={'Name'}></InputWithLabel>
-            <InputWithLabel title={'Email'} type='email'></InputWithLabel>
-            <InputWithLabel title={'Your Message'} type='text' isTextArea={true}></InputWithLabel>
-            <button className="btn btn-neutral w-fit self-end">
+            <InputWithLabel title={'Subject'}  onChange={(e) => setSubject(e.target.value)} type="text" value={subject}></InputWithLabel>
+            <InputWithLabel title={'Your Message'} type='text' isTextArea={true} onChange={(e) => setMessage(e.target.value)} value={message} ></InputWithLabel>
+            <button className="btn btn-neutral w-fit self-end" onClick={sendEmail}>
                 Submit
                 <SendHorizontal />
             </button>
