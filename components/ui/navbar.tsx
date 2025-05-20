e86fa8963f51246641d492ba75b481c32d36f0ed
naufalfathur <naufalfathur@gmail.com>
2025-05-20 23:41:19 +0100

    change cv to linkedin github

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -45,7 +45,14 @@ function Navbar() {
               </ul>
             </details>
           </li>
-          <li className='font-bold'><a href="./cv.pdf">Download CV</a></li>
+          <li className='font-bold'>  <a href='https://github.com/naufalfathur'>
+            <Image src="/github-icon.svg" height={20} width={20} alt={'github'} className='saturate-0 brightness-200' />
+          </a></li>
+          <li className='font-bold'>   <a href='https://www.linkedin.com/in/naufalfathur/'>
+            <Image src="/linkedin-icon.svg" height={20} width={20} alt={'github'} className='saturate-0 brightness-200' />
+          </a></li>
+
+          {/* <li className='font-bold'><a href="./cv.pdf">Download CV</a></li> */}
         </ul>
         <ul className="menu menu-horizontal px-1 hidden md:flex">
           {navigation.map((item, key) => (
@@ -58,7 +65,13 @@ function Navbar() {
               </Link>
             </li>
           ))}
-          <li className='font-bold'><a href="./cv.pdf">Download CV</a></li>
+          <li className='font-bold'>  <a href='https://github.com/naufalfathur'>
+            <Image src="/github-icon.svg" height={20} width={20} alt={'github'} className='saturate-0 brightness-200' />
+          </a></li>
+          <li className='font-bold'>   <a href='https://www.linkedin.com/in/naufalfathur/'>
+            <Image src="/linkedin-icon.svg" height={20} width={20} alt={'github'} className='saturate-0 brightness-200' />
+          </a></li>
+          {/* <li className='font-bold'><a href="./cv.pdf">Download CV</a></li> */}
           {/* <li><ThemeChanger/></li> */}
         </ul>
 
