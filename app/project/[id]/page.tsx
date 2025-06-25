cdd22d4c5aea86f97144acb7eb2057b67eabaff4
naufalfathur <naufalfathur@gmail.com>
2025-06-25 14:10:08 +0100

    fix params retriever next js 15

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -1,22 +1,25 @@
 import Contact from '@/components/app/contact';
 import Project from '@/components/project/project';
-import Footer from '@/components/ui/footer'
-import Navbar from '@/components/ui/navbar'
+import Footer from '@/components/ui/footer';
+import Navbar from '@/components/ui/navbar';
 import { projectList } from '@/public/data/projects';
 import { TProject } from '@/types';
-import React from 'react'
+import React from 'react';
 
-function ProjectPage( { params }: { params: { id: string } }) {
-  const filteredObjects = projectList.filter((project) => project.id === params.id)[0] as TProject
+type Props = {
+  params: Promise<{ id: string }>; // 👈 important: Promise
+};
+
+export default async function ProjectPage({ params }: Props) {
+  const { id } = await params; // 👈 await the params
+  const filteredObject = projectList.find((project) => project.id === id) as TProject | undefined;
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between  ">
-      <Navbar/>
-      {filteredObjects ? <Project project={filteredObjects}/> : <div>Invalid Project</div>}
-      <Contact/>
-      <Footer/>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Navbar />
+      {filteredObject ? <Project project={filteredObject} /> : <div>Invalid Project</div>}
+      <Contact />
+      <Footer />
     </main>
-  )
+  );
 }
-
-export default ProjectPage
