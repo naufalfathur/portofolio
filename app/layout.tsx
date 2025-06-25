285512dc3ff5d0a151e09a215983b06aa070515e
naufalfathur <naufalfathur@gmail.com>
2025-06-25 14:01:28 +0100

    gtm install

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const figtree = Figtree({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" >
+      <GoogleTagManager gtmId="GTM-5RNJH6NR" />
       <script src='script.js' defer></script>
       <body className={figtree.className}>
         {children}
