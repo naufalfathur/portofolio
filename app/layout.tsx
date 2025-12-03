fef0a01af2a3ce6f5d827a30d442edbb3607d3bc
naufalfathur <naufalfathur@gmail.com>
2025-12-03 18:10:22 +0000

    updated cta, added portfolio download

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,14 +26,14 @@ export default function RootLayout({
       {/* <script src='script.js' defer></script> */}
       <body className={figtree.className}>
         {children}
-        <div className='logo-floating fixed z-50 bottom-20 right-5 md:right-20 floating tooltip tooltip-open tooltip-top tooltip-warning cursor-pointer' data-tip="Click to work with me!">
-          <Link href={'/contact'}>
+        <div className='logo-floating fixed z-50 bottom-20 right-5 md:right-20 floating tooltip tooltip-open tooltip-top tooltip-warning cursor-pointer' data-tip="Download my portfolio">
+          <Link href={'https://assets.ctfassets.net/wtyk6zs1a32v/4dEsETfazkosTJVYAgfCZz/079b9a6fa47b14996c575e8965f489d0/naufal-wahid-tech-portfolio.pdf'} target="_blank" rel="noopener noreferrer">
             <Image
               alt='logo'
-              src={"/logo.png"}
+              src={"https://images.ctfassets.net/wtyk6zs1a32v/7yuWJhSidjiilmTThyLXu1/741afb285cbafe5d1626e28a1b6f0eea/portfolio.png"}
               width={0}
               height={0}
-              sizes='10vw'
+              sizes='15vw'
               quality={100}
               style={{ width: 'auto', height: 'auto' }}
               className='rotate-6 my-2'
